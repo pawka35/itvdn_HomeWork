49df5f862b593291a806bb61930bd68a284d32c1
skywalker35 <skywalker35@yandex.ru>
2019-11-06 16:57:24 +0300

    finish calculator

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
-/* Я честно пытался все это сделать без использования eval(), но не смог разобраться, как организовать
-приоритет выполнения операций. В качестве доказательств попыток оставляю закоменченный код */
+/*c eval() код получался раз в 10 короче..но говорят нельзя его использовать */
 
-// let actions = {
-//   "+": (a, b) => {
-//     return parseInt(a) + parseInt(b);
-//   },
-//   "-": (a, b) => {
-//     return parseInt(a) - parseInt(b);
-//   },
-//   "*": (a, b) => {
-//     return parseInt(a) * parseInt(b);
-//   },
-//   "/": (a, b) => {
-//     return parseInt(a) / parseInt(b);
-//   }
-// };
+/*определяем массив, элементы которого являются функциями и возвращают результаты 
+математических операций*/
 
+let actions = {
+  "+": (a, b) => {
+    return parseFloat(a) + parseFloat(b);
+  },
+  "-": (a, b) => {
+    return parseFloat(a) - parseFloat(b);
+  },
+  "*": (a, b) => {
+    return parseFloat(a) * parseFloat(b);
+  },
+  "/": (a, b) => {
+    return parseFloat(a) / parseFloat(b);
+  }
+};
+
+/*Переменная для хранения строки, которая складывается из введенных значений пользователем*/
 let enterString = "";
 
+/*Находим все кнопки, кроме равно, сброс, удалить символ*/ 
 let commonsButton = document.getElementsByClassName("common");
+/* при нажатии на них будет формировать строка ввода*/  
 for (let item of commonsButton) {
-  item.onclick = function() {
+  item.onclick = function() { // чтобы невозможно было начать ввод с оператора
     if (
       enterString == "" &&
       (this.value == "+" ||
@@ -32,27 +37,25 @@ for (let item of commonsButton) {
     }
 
     if (
-      (enterString.slice(-1) == "+"  ||
+      (enterString.slice(-1) == "+" || // чтобы нельзя было поставить 2 подряд оператора
         enterString.slice(-1) == "=" ||
         enterString.slice(-1) == "*" ||
-        enterString.slice(-1) == "/") 
-        &&
+        enterString.slice(-1) == "/") &&
       (this.value == "+" ||
         this.value == "-" ||
         this.value == "*" ||
-        this.value == "/")) 
-    {
+        this.value == "/")
+    ) {
       return;
     }
-
-    enterString += this.value;
-    document.getElementById("calcWindow").value += this.value;
+    enterString += this.value; // формируем строку
+    document.getElementById("displayUser").innerHTML  += this.value; // выводим строку
   };
 }
 
+/*Выбираем кнопку равно (=)*/ 
 let resultButtom = document.getElementById("result");
-resultButtom.onclick = function() {
-  let result;
+resultButtom.onclick = function() { //чтобы нельзя было нажать равно, пока нет знаком мат.операций в строке
   if (
     !enterString.includes("+") &&
     !enterString.includes("-") &&
@@ -61,42 +64,47 @@ resultButtom.onclick = function() {
   ) {
     return;
   }
-  result = eval(enterString);
-  // var expr = new RegExp("\\d+", "g");
-  // let digits = enterString.match(expr);
-  // expr = new RegExp("\\D", "g");
-  // let actionsArray = enterString.match(expr);
 
-  // let umn = actionsArray.map((element, index, a) => {
-  //   if (element == "*") {
-  //     return index;
-  //   }
-  // });
-
-  // for(let i in umn) {
-  //   if (typeof(umn[i])!== 'undefined')
-  //     alert(umn[i]);
-  // }
-
-  // for (let i = 0; i < actionsArray.length; i++) {
-  //   if (i == 0) {
-  //     result = actions[actionsArray[i]](digits[i], digits[i + 1]);
-  //   } else {
-  //     result = actions[actionsArray[i]](result, digits[i + 1]);
-  //   }
-  // }
-  document.getElementById("calcWindow").value += `=${result}\n`;
-  enterString = "";
+  /*для обеспечения приоритета операций, сначала выбираем в строке эелементы, которые умножаются, считаем результат,
+ заменяем в строке эти 2 элемента на результат, и по кругу, пока не кончится такие*/
+  findMultOrDiv('*');
+  findMultOrDiv('/');//тоже для деления
+  findMultOrDiv('+');// тоже для сложения
+  findMultOrDiv('-'); // тоже для вычитания
+ 
+  document.getElementById("displayUser").innerHTML  = `${enterString}\n`; // выволим результат 
+ // enterString = "";
 };
+
+/*Выбираем кнопку очистки поля...тут вроде все банально*/
 let resetButtom = document.getElementById("reset");
 resetButtom.onclick = () => {
   enterString = "";
-  document.getElementById("calcWindow").value = "";
+  document.getElementById("displayUser").innerHTML  = ``;
+
 };
 
-
+/*Кнопка удаления последнего введенного символа*/ 
 let deleteSymbol = document.getElementById("deleteSymbol");
 deleteSymbol.onclick = () => {
-  enterString = enterString.slice(0,enterString.length-1);
-  document.getElementById("calcWindow").value = enterString ;
+  enterString = enterString.slice(0, enterString.length - 1); // укорачиваем строку на 1 символ справа
+  document.getElementById("displayUser").innerHTML = enterString;
+
 };
+
+/*Функция для совершения математичских операций,в аргументе передается какую операцию ищем*/ 
+function findMultOrDiv(whatToFind){
+  /* составляем регулярку для поиска патерном (например: x+y*/ 
+  let expr = new RegExp(`\\d+\\.*\\d*\\${whatToFind}\\d+\\.*\\d*`, "g"); 
+  do { //пока регулярка что-то находит в нашей строке - выполняем цикл
+    let umn = enterString.match(expr);
+    if (umn === null) {
+      break; //если не удалось найти более вхождений, то прекращаем
+    }
+    umn.forEach(function(item) { //делим наш найденный шаблон на 2 операнда (операция нам передана)
+      let tmp = item.split(`${whatToFind}`);
+      let tmpRes = actions[`${whatToFind}`](tmp[0], tmp[1]); //вызываем необходимую операцию и передаем операнды
+      enterString = enterString.replace(item, tmpRes); // заменяем в строке операнды и операцию на результат операции
+    });
+  } while (true);
+}
