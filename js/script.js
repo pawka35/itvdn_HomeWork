29d1d024b25a05e9ad64c00ffa9d90eef5630433
pawka35 <pawka@pawka.ru>
2019-11-16 19:06:23 +0300

    done secundomer

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//для секундомера
+function Secundomer()  {
+  this.date = new Date(0, 0, 0, 0, 0, 0, 0),
+  this.interval,
+  this.startButton = document.getElementById("secStart"),
+  this.outDiv = document.getElementById('outPutDiv'),
+  this.curStamp = 0,
+
+  this.startButton.onclick = ()=>{
+    this.start();
+  },
+  this.stopButton = document.getElementById("secStop"),
+  this.stopButton.onclick = ()=>{
+    this.stop();
+  },
+
+  this.resetButton = document.getElementById("secReset"),
+  this.resetButton.onclick = ()=>{
+    this.reset();
+  },
+ 
+  this.main =  ()=> {
+    this.date.setMilliseconds(this.date.getMilliseconds()+100);
+    this.outDiv.innerHTML = `${this.date.getHours()}:${this.date.getMinutes()}:${this.date.getSeconds()}:${this.date.getMilliseconds()/100}`;
+  },
+
+  this.start =  function() {
+    this.interval = setInterval(this.main,100);
+    this.startButton.setAttribute("disabled", "disabled");
+  },
+
+  this.stop =  function(){
+    clearInterval(this.interval);
+    this.startButton.removeAttribute("disabled");
+  },
+  
+  this.reset =  function(){
+    this.date = new Date(0, 0, 0, 0, 0, 0, 0),
+    this.outDiv.innerHTML = '0:0:0:0';
+  }
+};
+
+let secundomer = new Secundomer();
