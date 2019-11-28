972f11d270ed1189f347b13b6b3dd0ffd830e82b
skywalker35 <skywalker35@yandex.ru>
2019-11-28 14:46:57 +0300

    hw done

--- js/script.js
+++ js/script.js
@@ -4,70 +4,77 @@ window.addEventListener("DOMContentLoaded", () => {
   const OUTPUTINFO_ID = "outputInfo";
   const TEXTAREA_ID = "textOutput";
   const FONT_SIZE_INPUT_ID = "fontSizeInput";
+
   //End of constants block
 
   init();
 
   function init() {
     let elements = {
+      //заполняем ассициативный массив элементами, которыми манипулируем
       colorChecker: document.getElementById(COLOCHECKER_ID),
       outputInfo: document.getElementById(OUTPUTINFO_ID),
       textArea: document.getElementById(TEXTAREA_ID),
       fntSize: document.getElementById(FONT_SIZE_INPUT_ID)
     };
-    
-    readCookies(elements);
-    addEvents(elements);
+
+    readCookies(elements); //функция для чтения значений из cookie
+    addEvents(elements); //определяем реакцию элементов на действия
   }
 
   function readCookies(elements) {
     if (window.localStorage.color) {
-      elements["textArea"].style.color = window.localStorage.color;
+      //если в куках есть значение цвета текста
+      elements["textArea"].style.color = window.localStorage.color; //присваиваем цвет текскту
+      elements["colorChecker"].value = window.localStorage.color; //выставляем значение инпута в соотв. с полученным
     }
     if (window.localStorage.fntSize) {
+      //тоже для значения размера щрифта
       elements["fntSize"].value = parseInt(window.localStorage.fntSize);
       elements["textArea"].style.fontSize = window.localStorage.fntSize;
     }
   }
 
   function addEvents(elements) {
-    let colorChecker = elements["colorChecker"];
+    let colorChecker = elements["colorChecker"]; //определяем элементы
     let outputInfo = elements["outputInfo"];
     let textArea = elements["textArea"];
     let fntSize = elements["fntSize"];
 
     fntSize.addEventListener("input", e => {
-      console.log(e.target.value);
-      textArea.style.fontSize = e.target.value + "px";
-      window.localStorage.fntSize = textArea.style.fontSize;
+      //при изменении инпута размера шрифта
+      textArea.style.fontSize = e.target.value + "px"; //делаем текст размера, как в инпуте
+      window.localStorage.fntSize = textArea.style.fontSize; // записываем текущее значение в куки
     });
 
     fntSize.addEventListener("keydown", e => {
-      console.log(e.keyCode);
-      let regular = /[0-9]/;
+      // при нажатии клавиши внутри инпута размера шрифта
+      let regular = /[0-9]/; // мы должны принимать только цифры
 
       switch (e.keyCode) {
-        case 38:
-          fntSize.value = +fntSize.value + 1;
-          fntSize.dispatchEvent(new Event("input"));
+        case 38: // если нажали кнопку вверх
+          fntSize.value = +fntSize.value + 1; //увеличиваем размер на 1
+          fntSize.dispatchEvent(new Event("input")); //имитируем этого значения с клавиатуры
           break;
-        case 40:
+        case 40: //тоже для кнопки вниз
           fntSize.value -= 1;
           fntSize.dispatchEvent(new Event("input"));
           break;
         default:
+          //для всех остальных кнопок
           if (!regular.test(e.key) && e.key != "Backspace") {
-            e.preventDefault();
+            //если не цифра и не стираем символ
+            e.preventDefault(); //предотвращаем действие
           }
           break;
       }
     });
 
     colorChecker.addEventListener("change", e => {
-      console.log(colorChecker.value);
-      outputInfo.innerHTML = colorChecker.value;
-      textArea.style.color = colorChecker.value;
-      window.localStorage.color = colorChecker.value;
+      //изменение значения инпута выбора цвета
+      //   console.log(colorChecker.value);
+      textArea.style.color = colorChecker.value; //делаем текст выбранного цвета
+      window.localStorage.color = colorChecker.value; //записываем в куки
     });
   }
 });
