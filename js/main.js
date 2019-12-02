755f0d1d1ff6380fb7df5a2ac8158d81c3eb6177
pawka35 <pawka@pawka.ru>
2019-12-02 21:56:05 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
 window.addEventListener("DOMContentLoaded", () => {
-  // <li id="exc1">Задание 1</li>
-  //   <li id="exc2">Задание 2</li>
-  //   <li id="exc3">Задание 3</li>
-  let activeWindow = "";
-
-  let allexc = { "1": exc1, "2": exc2, "3": exc3 };
-  let btns = [...document.getElementsByClassName("mnBtn")];
-  // console.log(btns);
+  let activeWindow = ""; //активное в данный момент окно задания
+  let allexc = { "1": exc1, "2": exc2, "3": exc3 }; //функции для каждого задания
+  let btns = [...document.getElementsByClassName("mnBtn")]; //кнопки вызова заданий
 
   btns.forEach(item => {
+    //навешиваем действия на кнопки выбора заданий
     item.addEventListener("click", e => {
-      let nb = e.target.id.replace("mn_exc", "");
-      allexc[nb]();
+      let nb = e.target.id.replace("mn_exc", ""); //определяем какое задания надо запустить
+      // console.log("exc" + nb);
+      let thisWind = document.getElementById("exc" + nb); //определяем какое окно надо открыть
+      if (thisWind != activeWindow) {
+        //если это окно еще не открыто, то открываеми
+        closeActiveWindow(thisWind);
+      } else {
+        //если окно уже и так открыто, то ничего не делаем
+        return;
+      }
+      allexc[nb](); //запускаем выполнения соответствующего задания
+      btns.forEach(item => item.classList.remove("exc-checker-checked")); //снимаем метку нажатой со всех кнопок
+      e.target.classList.add("exc-checker-checked"); //ставим метку нажатия на нажатую кнопку
     });
   });
 
+  function closeActiveWindow(newWind) {
+    //функция открытия окна задания
+    //закрываем открытое и открываем переданное
+    if (activeWindow) {
+      //если какое-то окно отрыто, то закрываем его
+      activeWindow.style.display = "none";
+    }
+    activeWindow = newWind; //запоминаем окно которое открываем, как открытое
+    activeWindow.style.display = "grid"; //собственно показываем его
+  }
+
   function preload() {
+    //функция предзагрузки изображений (разобрана в примерах)
     let images = [];
     [...preload.arguments].forEach(item => {
       //   console.log(item);
@@ -26,41 +45,32 @@ window.addEventListener("DOMContentLoaded", () => {
     return images;
   }
 
-  function closeActiveWindow(newWind) {
-    //закрываем открытое и открываем переданное
-    if (activeWindow) {
-      activeWindow.style.display = "none";
-    }
-    activeWindow = newWind;
-    activeWindow.style.display = "grid";
-  }
-
   function exc1() {
-    let thisWind = document.getElementById("exc1");
-    closeActiveWindow(thisWind);
-
+    //задание 1
     let images = preload(
+      //предзагружаем нашик артинки в массив
       "images/exc1/1.png",
       "images/exc1/2.png",
       "images/exc1/3.png"
     );
-    let imgDiv = document.getElementById("exc1-image");
-    let exc1Btn = [...document.getElementsByClassName("exc1-btn")];
+    let imgDiv = document.getElementById("exc1-image"); //ищем див где отображать картинку
+    let exc1Btn = [...document.getElementsByClassName("exc1-btn")]; //находим все кнопки
 
     exc1Btn.forEach(item => {
+      //для каждой кнопки навешиваем событие на нажатие
       item.addEventListener("click", e => {
-        let needImg = e.target.id.replace("exc1-btn", "");
-        imgDiv.style.backgroundImage = `url("${images[needImg - 1].src}")`;
+        let needImg = e.target.id.replace("exc1-btn", ""); //определяем какое изображение нам надо открыть
+        imgDiv.style.backgroundImage = `url("${images[needImg - 1].src}")`; //отображаем
       });
     });
   }
 
   function exc2() {
-    let thisWind = document.getElementById("exc2");
-    closeActiveWindow(thisWind);
-
-    let activeDiv = "";
+    //упражнение 2
+    const BIG_IMG_FOLDER = "images/ecx2"; //папка где лежат большие изображения
+    let activeDiv = ""; //текущее активное изображение
     let previewImages = preload(
+      //предзагружаем маленькие картинки
       "images/ecx2/preview/preview_1.jpg",
       "images/ecx2/preview/preview_2.jpg",
       "images/ecx2/preview/preview_3.jpg",
@@ -69,62 +79,75 @@ window.addEventListener("DOMContentLoaded", () => {
 
     let previewImageDivs = [
       ...document.getElementsByClassName("exc2-image-preview")
-    ];
+    ]; //находим все дивы,где должны быть превьюхт
     for (let i = 0; i < previewImages.length; i++) {
+      //размещаем превью
       previewImageDivs[
         i
       ].style.backgroundImage = `url("${previewImages[i].src}")`;
     }
 
     previewImageDivs.forEach(item => {
+      //на каждый див с превью навешиваем действие на нажатие
       item.addEventListener("click", e => {
-        let bigDiv = document.getElementById("exc2-image-bigView");
-        let bigDivImg = document.getElementById("exc2-image-bigView-image");
+        let bigDiv = document.getElementById("exc2-image-bigView"); //находим наш див для отображения большое картинки
+        let bigDivImg = document.getElementById("exc2-image-bigView-image"); //находим в нем место для отображения
         if (activeDiv == e.target.id) {
+          //если и так открыто это изображение, то нафиг
           return;
         } else {
           activeDiv = e.target.id;
-          bigDivImg.style.backgroundImage = `url("${
-            previewImages[e.target.id.replace("exc2-image", "") - 1].src
-          }")`;
-          bigDiv.classList.add("b-show");
-          let cls = document.getElementById("exc2-image-bigView-close");
+          // console.log(e.target.id);
+          bigDivImg.style.backgroundImage = `url("${BIG_IMG_FOLDER}/${e.target.id.replace(
+            "exc2-image",
+            ""
+          )}.jpg")`; //отображаем большое изображение
+
+          bigDiv.classList.add("b-show"); //плавно показываем большой див (см. css)
+          let cls = document.getElementById("exc2-image-bigView-close"); //кнопка закрыть в большое диве
           cls.addEventListener("click", e => {
+            //при нажатии на кнопку закрытий, див закрывается. Ваш К.О.
             bigDiv.classList.remove("b-show");
           });
         }
       });
     });
-
-    // document.getElementById('exc2-image-bigView').classList.add('b-show');
-    // D:\ITVDN Cours\itvdn_HomeWork\images\ecx2\preview\preview_1.jpg
   }
 
   function exc3() {
-    let valueArr = [];
-    let colors = [];
-    let thisWind = document.getElementById("exc3");
-    closeActiveWindow(thisWind);
+    //упражнение 3
+    let valueArr = []; //массив для значений
+    let colors = []; //массив для цветов
 
-    let addToTblBtn = document.getElementById("ecx3-addBtn");
-    let valueInp = document.getElementById("exc3-valInput");
-    let nameInp = document.getElementById("exc3-NameInput");
-    let tableBody = document
+    let addToTblBtn = document.getElementById("ecx3-addBtn"); //кнопка внесения в таблицу из инпутов
+    let valueInp = document.getElementById("exc3-valInput"); //инпут значения
+    let nameInp = document.getElementById("exc3-NameInput"); //инпут названия
+    let tableBody = document //находим тело нашей таблицы
       .getElementById("exc3-table")
       .getElementsByTagName("tbody")[0];
 
-    let radioGraphs = [...document.getElementsByName("graph")];
+    valueInp.addEventListener("keydown", e => {
+      //не даем вводит в инпут значений ничего кроме цифр(ну и стирать можно)
+      if (!/[0-9]/.test(e.key) && e.key != "Backspace") {
+        e.preventDefault();
+      }
+    });
+
+    let radioGraphs = [...document.getElementsByName("graph")]; //наши радио-буттоны переключения вида графика
     radioGraphs.forEach(item => {
+      //вешаем рекакцию на переключение радио-баттонов
       item.addEventListener("change", e => {
         if (valueArr.length == 0) {
+          //если значений нет, то ничего не делаем (график не построить один фиг)
           return;
         }
-        changeGraphType(e.target.id);
+        changeGraphType(e.target.id); //запускаем функция отображения графика
       });
     });
 
     function changeGraphType(radioBnt) {
-      console.log("cha", radioBnt);
+      //функция отображения графика
+      //смотрим какой график нужен и вызываю функцию рисования конктретного типа
       switch (radioBnt) {
         case "line":
           createLeaner("convasContainer", valueArr, 500, 200, colors);
@@ -144,30 +167,37 @@ window.addEventListener("DOMContentLoaded", () => {
 
     addToTblBtn.addEventListener("click", e => {
       //добавляем значение в таблицу
-      var newRow = tableBody.insertRow(tableBody.rows.length);
-      var newCell = newRow.insertCell(0);
+      var newRow = tableBody.insertRow(tableBody.rows.length); //вставляем новую строку в таблицу
+      var newCell = newRow.insertCell(0); //делаем новые ячейки
       var newCell2 = newRow.insertCell(1);
       var newCell3 = newRow.insertCell(2);
       if (nameInp.value == "") {
-        nameInp.value = `Параметр ${valueArr.length + 1}`;
+        if (valueInp.value == "") {
+          //не позволяем отправить пустое значение в таблицу
+          return;
+        }
+        nameInp.value = `Параметр ${valueArr.length + 1}`; //если не ввели имя, то сами генерим
       }
-      var newText = document.createTextNode(nameInp.value);
+
+      var newText = document.createTextNode(nameInp.value); //значение для ячеек
       var newText2 = document.createTextNode(valueInp.value);
 
-      valueArr.push(parseInt(newText2.data));
-      let newColor = getRandomColor();
-      colors.push(newColor);
-      newCell.appendChild(newText);
+      valueArr.push(parseInt(newText2.data)); //добавляем значение в нащ массив значений, для передачи на построение
+      let newColor = getRandomColor(); //генерим случайный цвет
+      colors.push(newColor); //также формаируем массиф цвето для передачи на построение
+      newCell.appendChild(newText); //тут добавляем нащи значения в ячейки
       newCell2.style.textAlign = "center";
       newCell2.appendChild(newText2);
       newCell3.style.backgroundColor = newColor;
       valueInp.value = "";
       nameInp.value = "";
-      let radioGraph = document.querySelector('input[name="graph"]:checked');
-      changeGraphType(radioGraph.id);
+      let radioGraph = document.querySelector('input[name="graph"]:checked'); //это мы не проходили, но лень было писать
+      //цикл перебора, поэтому сразу определяем выбранный радио-буттон
+      changeGraphType(radioGraph.id); //заново перересовываем график (у нас новые значения)
     });
   }
 
+  //рисуем линейный график...остальные нарисова Петру...поэтому сам написал только этот тип
   function createLeaner(canvasId, data, width, height, colors) {
     const canvas = document.getElementById("canvasContainer");
     canvas.width = width + 20;
@@ -177,18 +207,16 @@ window.addEventListener("DOMContentLoaded", () => {
       return Math.max(cur, prev);
     });
     let koef = (height - 25) / max; //коэфициент пропорционального уменьшения
-    console.log(koef);
+    // console.log(koef);
 
-    let shag = Math.floor(parseInt(width) / data.length);
+    let shag = Math.floor(parseInt(width) / data.length);//расстояние между значениями
     let context = canvas.getContext("2d");
     context.beginPath();
-    context.moveTo(10, height - data[0] * koef);
+    context.moveTo(10, height - data[0] * koef); //первое значение ставим в точке 10 по иксу 
     context.strokeText(data[0], 10, height - data[0] * koef - 15);
-    for (let i = 1; i < data.length; i++) {
+    for (let i = 1; i < data.length; i++) { //пробегаемся по всему массиву со значениями и расставляем точки
       context.lineTo(i * shag, height - data[i] * koef);
-      context.strokeText(data[i], i * shag, height - data[i] * koef - 15);
-      // context.textBaseline = "hanging";
-      // console.log(height, height - data[i] * koef);
+      context.strokeText(data[i], i * shag, height - data[i] * koef - 15); //подписываем значения
     }
     context.stroke();
 
