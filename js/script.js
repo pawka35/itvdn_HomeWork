3e5a9c94d76abde6659217d9226bc094ee6664ca
skywalker35 <skywalker35@yandex.ru>
2019-12-06 16:07:40 +0300

    add some more

--- js/script.js
+++ js/script.js
@@ -2,18 +2,57 @@ function _get(id) {
   return document.getElementById(id);
 }
 
+const CORSE_HACK = "https://cors-anywhere.herokuapp.com/";
+// let chatActivated = false;
+
 window.addEventListener("DOMContentLoaded", () => {
   //const block
   const USERS_ENDPOINT = "https://gorest.co.in/public-api/users";
+  const NEWS_API = "72e5bc2b78bc4b8ea87cc9eabf5b372f";
+  const NEWS_ENDPOINT = `https://newsapi.org/v2/top-headlines?country=ru&apiKey=${NEWS_API}`;
 
-  const USERS_LIST =
-    //end const block
-    init();
+  //   const USERS_LIST;
+
+  //end const block
+  init();
   function init() {
-    getUsers(USERS_ENDPOINT, "GET");
+    getFromAPI(USERS_ENDPOINT, "GET", showFriendsList);
+    getFromAPI(NEWS_ENDPOINT, "GET", showNews);
   }
 
-  function getUsers(url, method) {
+  function showNews(info) {
+    console.log(info.articles[0]);
+    info.articles.forEach(item => {
+      console.log(item.title);
+      let newsDiv = _get("main-news");
+      newsDiv.classList.add("news-container");
+      let newArticle = document.createElement("div");
+      newArticle.classList.add('news-article');
+      let title = document.createElement("div");
+      title.classList.add('news-title');
+      let desc = document.createElement("div");
+      desc.classList.add('news-desc');
+
+      let url = document.createElement("div");
+      url.classList.add('news-url');
+
+      let photo = document.createElement("div");
+      photo.classList.add('news-photo');
+
+      desc.innerHTML = item.description;
+      title.innerHTML = item.title;
+      url.innerHTML = `<a href="${item.url}" target="_blank">Читать в источнике</a>`;
+      photo.style.backgroundImage = `url(${item.urlToImage})`;
+
+      newArticle.appendChild(title);
+      newArticle.appendChild(photo);
+      newArticle.appendChild(desc);
+      newArticle.appendChild(url);
+      newsDiv.appendChild(newArticle);
+    });
+  }
+
+  function getFromAPI(url, method, showFunction) {
     let res;
     let xhr = new XMLHttpRequest(); // Создание объекта для HTTP запроса.
     xhr.open(method, url, true);
@@ -25,144 +64,142 @@ window.addEventListener("DOMContentLoaded", () => {
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200) {
         res = JSON.parse(xhr.response);
-
-        let userList = _get("main-friends");
-        for (let i = 0; i < 10; i++) {
-          //   console.log(res.result[i]);
-          let newFriendDiv = document.createElement("div");
-          let newP = document.createElement("div");
-          let newPhoto = document.createElement("div");
-          let newActive = document.createElement("div");
-          newFriendDiv.classList.add("friends");
-          newP.classList.add("friends-name");
-          newPhoto.classList.add("friends-avatar");
-          newActive.classList.add("friends-status");
-
-          // console.log(res.result[i].status)
-          newP.innerHTML = res.result[i].first_name + res.result[i].last_name;
-          newFriendDiv.dataset.id = res.result[i].id;
-
-          if (res.result[i].status == "active") {
-            newActive.style.backgroundColor = "red";
-          } else {
-            newFriendDiv.classList.add("active");
-            newActive.style.backgroundColor = "green";
-            newFriendDiv.addEventListener("click", () => {
-              goToChat(newFriendDiv);
-            });
-            // goToChat(newFriendDiv);
-          }
-          // userList.innerHTML += res.result[i].first_name + res.result[i].last_name+'<br>';
-          newFriendDiv.appendChild(newPhoto);
-          newFriendDiv.appendChild(newP);
-          newFriendDiv.appendChild(newActive);
-          newPhoto.style.backgroundImage = `url(${res.result[i]._links.avatar.href})`;
-          userList.appendChild(newFriendDiv);
-        }
-        // addToHtml(res.result)
+        showFunction(res); //передаем ответ в функцию для отображения
       }
       if (xhr.status == 401) {
         res = JSON.parse(xhr.response);
-        // console.log(res);
-        // addToHtml(res.result)
       }
-
       let loader = document.getElementsByClassName("loader")[0];
       loader.style.display = "none";
     };
   }
 });
 
+function showFriendsList(res) {
+  let userList = _get("main-friends");
+  for (let i = 0; i < 10; i++) {
+    let newFriendDiv = document.createElement("div");
+    let newP = document.createElement("div");
+    let newPhoto = document.createElement("div");
+    let newActive = document.createElement("div");
+    newFriendDiv.classList.add("friends");
+    newP.classList.add("friends-name");
+    newPhoto.classList.add("friends-avatar");
+    newActive.classList.add("friends-status");
+    newP.innerHTML = res.result[i].first_name + " " + res.result[i].last_name;
+    newFriendDiv.dataset.id = res.result[i].id;
+
+    if (res.result[i].status == "active") {
+      newActive.style.backgroundColor = "red";
+    } else {
+      newFriendDiv.classList.add("active");
+      newActive.style.backgroundColor = "green";
+      newFriendDiv.addEventListener("click", () => {
+        goToChat(newFriendDiv);
+      });
+    }
+    newFriendDiv.appendChild(newPhoto);
+    newFriendDiv.appendChild(newP);
+    newFriendDiv.appendChild(newActive);
+    newPhoto.style.backgroundImage = `url(${res.result[i]._links.avatar.href})`;
+    userList.appendChild(newFriendDiv);
+  }
+}
+
 function goToChat(frDiv) {
-    // downloadScript();
-  //   frDiv.addEventListener("click", () => {
-//   console.log('we in to chat');
-//   console.log(frDiv.dataset.id); 
- let chat = _get("main-news");
-//     chat.innerHTML = `<script src="js/chat.js"></script>`;
+  //открываем страницу с чатом с другом
+  window.scrollTo(0, 0);
+  let chat = _get("main-news");
   let xhr = new XMLHttpRequest(); // Создание объекта для HTTP запроса.
-  
+
   xhr.open("GET", "_chat.html", false);
-  xhr.send(); 
+  xhr.send();
   if (xhr.readyState === xhr.DONE) {
-    console.log(xhr.responseText);
+    // console.log(xhr.responseText);
     chat.innerHTML = xhr.responseText;
   }
-   test();
+  activateChat(frDiv);
 
+  //   function getMusic() {
+  //     var data = null;
+  //     var xhr = new XMLHttpRequest();
+  //     xhr.addEventListener("readystatechange", function() {
+  //       if (this.readyState === this.DONE) {
+  //         console.log(this.responseText);
+  //       }
+  //     });
 
-//   xhr.onreadystatechange = function() {
-   
-//     // if (xhr.readyState == 4 && xhr.status == 200) {
-//     //   res = xhr.response;
-     
-//     // // console.log(res);
-//     //   chat.innerHTML = res;
-//     //   test();
-//     }
-  };
+  //     xhr.open(
+  //       "GET",
+  //       `${CORSE_HACK}https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem`
+  //     );
+  //     xhr.setRequestHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com");
+  //     xhr.setRequestHeader(
+  //       "x-rapidapi-key",
+  //       "66002bd060msh0f2312223da9155p12aaf4jsn0f415817bea5"
+  //     );
+  //     xhr.send(data);
+  //   }
 
-//   xhr.open();
-  //   });
+  function activateChat(frDiv) {
+    //заполняем страницу информацией о друге
+    let bigFoto = document.getElementById("chat-friendInfo-Foto");
+    let ft = frDiv.getElementsByClassName(
+      "friends-avatar"
+    )[0]; /*иногда показывается не тот аватар
+    т.к. сервис по одной и той же ссылке выдает разные аватары..с нормальном api такой фигни не 
+    будет*/
+    let name = frDiv.getElementsByClassName("friends-name")[0];
+    document.getElementById("chat-friendInfo-Name").innerHTML = name.innerHTML;
+    bigFoto.style.backgroundImage = ft.style.backgroundImage;
 
+    let messageInput = _get("chat-input-textArea");
+    let sendBTN = _get("sendMessage");
+    let outDiv = _get("chat-output");
 
+    sendBTN.addEventListener("click", () => {
+      sendMessage();
+    });
 
+    messageInput.addEventListener("keydown", e => {
+      if (e.keyCode == 13 && e.ctrlKey == true) {
+        sendMessage();
+      }
+    });
 
+    function sendMessage() {
+      //фукнция отправки сообщения боту
+      let message = messageInput.value;
+      let xhr = new XMLHttpRequest();
+      //если не работает из-за corse, раскоментируйте url и закоментируйте текущий
+      //let url = `${CORSE_HACK}http://aiproject.ru/api/`;
+      let url = `http://aiproject.ru/api/`;
 
+      //let url = 'https://xu.su/api/send'; //another chat bot
+      //question = `{'uid':"e06a5358-560c-4def-b2d0-6ec87e7a443a",'bot':"main",'text':"${question}"}`;
 
-function getMusic() {
-  var data = null;
-
-  var xhr = new XMLHttpRequest();
-  // xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function() {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
+      console.log(url);
+      xhr.open("POST", url, true);
+      question = { ask: `${message}`, userid: "Pawka", key: "" };
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          outDiv.innerHTML = outDiv.innerHTML.replace(
+            "Набирает сообщение...",
+            ""
+          );
+          outDiv.innerHTML += `<font color="green">${
+            JSON.parse(xhr.response).aiml
+          }</font><br>`;
+          outDiv.scrollTop = outDiv.scrollHeight;
+        }
+      };
+      messageInput.value = "";
+      xhr.send(`query=${JSON.stringify(question)}`);
+      outDiv.innerHTML += message + "<br>";
+      outDiv.innerHTML += "&emsp;Набирает сообщение...";
+      outDiv.scrollTop = outDiv.scrollHeight;
     }
-  });
-
-  xhr.open(
-    "GET",
-    `${CORSE_HACK}https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem`
-  );
-  xhr.setRequestHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com");
-  xhr.setRequestHeader(
-    "x-rapidapi-key",
-    "66002bd060msh0f2312223da9155p12aaf4jsn0f415817bea5"
-  );
-
-  xhr.send(data);
-}
-
-function talk(message) {
-  let xhr = new XMLHttpRequest();
-  // let url = `${CORSE_HACK}http://aiproject.ru/api/`
-  let url = `http://aiproject.ru/api/`;
-  //let url = 'https://xu.su/api/send'; //another chat bot
-  // pp = {'uid':"e06a5358-560c-4def-b2d0-6ec87e7a443a",'bot':"main",'text':"Спать будешь?"};
-
-  console.log(url);
-  xhr.open("POST", url, true);
-  pp = { ask: `${message}`, userid: "654321", key: "" };
-  jpp = JSON.stringify(pp);
-  console.log(JSON.stringify(pp));
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      outDiv.innerHTML += JSON.parse(xhr.response).aiml;
-    }
-  };
-  console.log(jpp);
-
-  xhr.send(`query=${jpp}`);
-}
-
-
-function test(){
-    let rr = _get('chat-input');
-    rr.addEventListener('input',()=>{
-        console.log('sdfs');
-    })
-    
+    // chatActivated = true;
+  }
 }
