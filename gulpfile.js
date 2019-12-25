73309f4ceeb404e1083ab41aa732835c6f55bcf8
pawka35 <pawka@pawka.ru>
2019-12-25 21:04:54 +0300

    gulp and module added

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+//Подключаем локальные пакеты
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+
+//Определение задачи по умолчанию в GULP
+gulp.task("default", function() {
+  console.log(10);
+});
+
+gulp.task("sassToCSS", function(done) {
+  gulp
+    .src("./dev/style.scss")
+    .pipe(
+      sass({
+        errorLogToConsole: true
+        // outputStyle: 'compressed'
+      })
+    )
+    .on("error", console.error.bind(console))
+    // .pipe(rename({suffix: '.min'}))
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(gulp.dest("./css/"));
+    done();
+});
+
+//Отслеживание изменений
+gulp.task("scss:watch", function() {
+  //Находим папку и отмечаем, за каким файлом необходимо наблюдать, если он найден - выполняем задание "scss"
+  gulp.watch("./dev/**/*.scss", gulp.series("sassToCSS"));
+});
+
+// gulp.task('watch', function() {
+//     gulp.watch('app/css/*.css', gulp.series('styles'));
+//     gulp.watch('app/js/*.js', gulp.series('scripts'));
+//     gulp.watch('app/img/*', gulp.series('images'));
+//   });
