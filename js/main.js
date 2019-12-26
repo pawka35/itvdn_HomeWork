0d09964d3702917b32ad04eebb446b869f83d6ed
pawka35 <pawka@pawka.ru>
2019-12-26 22:40:52 +0300

    add header, carousel(needFix)

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function(){
+
+    var $slides = [...document.querySelectorAll('.slide')]; 
+    var currentSlide = 0;
+    
+    TweenLite.set($slides.filter(index => index > 0), {left:"960px"});    
+    TweenLite.delayedCall(1, nextSlide);                
+
+    function nextSlide(){                   
+            TweenLite.to( $slides[currentSlide], 1, {left:"-960px"} );       
+            
+            if (currentSlide < $slides.length - 1) {
+                currentSlide++;
+            }
+            else {
+                currentSlide = 0;
+            }
+                                                
+            TweenLite.fromTo( $slides[currentSlide], 1, {left: "960px"}, {left:"0px"} );
+            TweenLite.delayedCall(4, nextSlide);                                
+    }
+    
+    
+})();
